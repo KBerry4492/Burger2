46821980b404d1778e49f70447d0bbb4e369f0ce
KBerry4492 <kmb23@comcast.net>
2017-11-10 22:22:08 -0500

    Port issues

--- models/burgers2.js
+++ models/burgers2.js
@@ -1,26 +1,22 @@
 module.exports = function(sequelize, DataTypes) {
-  var Burgers = sequelize.define('Burgers', {
+  var Burger = sequelize.define('Burger', {
 
-        burger_name: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          
-          validate: {
-            notEmpty: true,
-          
-          }
-        }, //end burger name
+	    burger_name: {
+	      type: DataTypes.STRING,
+	      allowNull: false,
+	      
+	      validate: {
+	        notEmpty: true,
+	      
+	      }
+	    }, //end burger name
 
-        eaten: {
-          type: DataTypes.BOOLEAN,
-          defaultValue: false,
-        },
+	    eaten: {
+	      type: DataTypes.BOOLEAN,
+	      defaultValue: false,
+	    },
 
-      }, //end table
-
-      {
-        underscored: true,
-        freezeTableName: true,
-      } //cutlery
-    )
-  return Burgers;
+	  } //end table
+	);
+  return Burger;
+};
